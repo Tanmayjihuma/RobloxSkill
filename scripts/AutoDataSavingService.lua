5e17ba220b8f53c9f174edd50687caff51aa6bbd
TANMAY Agarwal <tanmayagarwal072007@gmail.com>
2026-06-12 21:56:27 +0530

    Update AutoDataSavingService.lua

--- scripts/AutoDataSavingService.lua
+++ scripts/AutoDataSavingService.lua
@@ -9,16 +9,19 @@ local OrdinaryDataService = require(game.ReplicatedStorage.Services.DataService.
 local Config = ReplicatedStorage:WaitForChild("Config")
 local DefaultData = require(Config:WaitForChild("DefaultData"))
 local AutoDataSavingService = {}
-local DATA_TYPE_WIN = "LEADERSTATS_DATA_WIN"
-local DATA_TYPE_STREAK = "LEADERSTATS_DATA_STREAK"
+local DATA_TYPE_EXAMPLE_ATTRIBUTE = "ATTRIBUTE_EXAMPLE"
+-- we need different for leaderstate cuz we use ordianrary data store service that stores numbers not values 
+local DATA_TYPE_EXAMPLE_LEADERSTATE_1= "LEADERSTATS_EXAMPLE_1"
+local DATA_TYPE_EXAMPLE_LEADERSTATE_2 = "LEADERSTATS_EXAMPLE_2"
 
 AutoDataSavingService.SaveLeaderstatsData = function(player)
 	
 	if _G.DataLoaded[player.UserId.."LEADERSTATS_DATA_LOADED"] then
-		local Wins = player.leaderstats.Wins.Value
-		local Streak = player.leaderstats.Streak.Value
-		OrdinaryDataService.savePlayerData(DATA_TYPE_WIN , 6, false , true , player , Wins)
-		OrdinaryDataService.savePlayerData(DATA_TYPE_STREAK , 6, false , true , player , Streak)
+		local EXAMPLE_DATA_1 = player.leaderstats.EXAMPLE_DATA_1.Value
+		local EXAMPLE_DATA_2 = player.leaderstats.EXAMPLE_DATA_2.Value
+		OrdinaryDataService.savePlayerData(DATA_TYPE_EXAMPLE_LEADERSTATE_1 , 6, false , true , player , EXAMPLE_DATA_1)
+		OrdinaryDataService.savePlayerData(DATA_TYPE_EXAMPLE_LEADERSTATE_2 , 6, false , true , player , EXAMPLE_DATA_2)
+		
 		_G.DataLoaded[player.UserId.."LEADERSTATS_DATA_LOADED"] = nil
 	end
 end 
@@ -33,18 +36,16 @@ AutoDataSavingService.SaveAttributesData= function(player)
 		end
 	end
 
-	DataService.savePlayerData("PlayerAttributeData", 4, false, true, player, dataToSave)
+	DataService.savePlayerData(DATA_TYPE_EXAMPLE_ATTRIBUTE, 4, false, true, player, dataToSave)
 end
 
+-- FOR ITEMS OR OTHER DATA (WHEN DATA CONTAINS TABLE)
 AutoDataSavingService.SaveItemsData = function(player)
 	if _G.DataLoaded[player.UserId.."ITEMS_DATA_LOADED"] then
 		local dataToSave = {
-			Chair = player:GetAttribute("OwnedChairs") ,
-			Death = player:GetAttribute("OwnedDeath") ,
-			EquippedChair = player:GetAttribute("EquippedChair"),
-			EquippedDeath = player:GetAttribute("EquippedDeath") 
+			Chair = player:GetAttribute("EXAMPLE_ITEMS") ,
+			Death = player:GetAttribute("EXAMPLE_EFFECTS") ,
 		}
-
 		DataService.savePlayerData("PlayeritemsData", 5, false, true, player, dataToSave)
 	end
 end
